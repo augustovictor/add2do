b73ca00a21b4a20702118097b158148ff250e7d0
Victor <victoraweb@gmail.com>
2016-05-24 09:22:48 -0300

    TextWatcher added to todoFragment

--- app/src/main/java/com/augustovictor/add2do/Fragments/TodoFragment.java
+++ app/src/main/java/com/augustovictor/add2do/Fragments/TodoFragment.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,6 +48,23 @@ public class TodoFragment extends Fragment {
 
         mTitle = (EditText) v.findViewById(R.id.et_task_title);
         mTitle.setText(mTodo.getmTitle());
+
+        mTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mTodo.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         mDone = (CheckBox) v.findViewById(R.id.ck_task_done);
         mDone.setChecked(mTodo.ismDone());
